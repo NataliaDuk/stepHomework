0e4a7027bbf61eb195e72be5402596df743aa726
Zhiznevskiy_Yura <zhizhnevskiy@yandex.ru>
2020-12-30 11:50:51 +0300

    doing

---
+++ 19/calc.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Сalculator</title>
+</head>
+<body>
+    <?php
+
+$answer1 =  $_POST["q1"];
+$answer2 =  $_POST["q2"];
+$answer3 =  $_POST["q3"];
+
+$numbers = [
+    "ноль" => 0,
+    "один" => 1,
+    "два" => 2,
+    "три" => 3,
+    "четыре" => 4,
+    "пять" => 5,
+    "шесть" => 6,
+    "семь" => 7,
+    "восемь" => 8,
+    "девять" => 9,
+    "десять" => 10
+];
+
+echo $numbers[$answer1] . $answer3 . $numbers[$answer2];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+</body>
+</html>
