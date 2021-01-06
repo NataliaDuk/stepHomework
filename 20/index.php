293877d2627efcb4eca93fe9021c55971b3001a4
zhizhnevskiy <zhizhnevskiy@yandex.ru>
2021-01-06 14:58:11 +0300

    doing 20

--- 20/index.php
+++ 20/index.php
@@ -8,15 +8,13 @@
 </head>
 <body>
 
-<div>
-
-<h1>Задача о зёрнах на шахматной доске:</h1>
+<h1>Задача о зёрнах на шахматной доске</h1>
 
 <span>математическая задача, в которой вычисляется, сколько будет зёрен на шахматной доске,</span><br>
 <span>если класть на каждую следующую клетку доски вдвое больше зёрен, чем на предыдущую, начиная с одного.</span><br>
 
-<form class="question" action="result.php" method="POST">
-    <h2>Создайте свою шахматную доску:</h2>
+<form action="result.php" method="POST">
+    <h2>Создайте свою шахматную доску</h2>
     <span>Количество ячеек по ширине:</span>
     <input type="number" value="1" min="1" max="999" name="q1">
     <br>
@@ -29,10 +27,12 @@
     <br><br>
     <input type="submit" value="выполнить">
 </form>
-
+<br>
+<img src="01.jpg" alt="альтернативный текст">
+<br>
 <footer>
     &copy; Zhizhnevskiy Yuriy 2021
 </footer>
-</div>
+
 </body>
 </html>
--- 20/result.php
+++ 20/result.php
@@ -8,33 +8,56 @@
 </head>
 <body>
 
+
+<h1>Задача о зёрнах на шахматной доске</h1>
+<h2>Созданная Вами доска</h2>
+
+<table>
 <?php
 $answer1 =  $_POST["q1"];
 $answer2 =  $_POST["q2"];
 $answer3 =  $_POST["q3"];
 
-$countChess = $answer1 * $answer2;
+function countCorn($a, $b){
+    $countChess = $a * $b;
+    $result = 1;
+    $sum = 0;
+    $i = 1;
+    while ($i<=$countChess){
+        $sum += $result;
+        $result *= 2;
+        $i++;
+    }
+    return $sum;
+}
 
+$countCorn = countCorn($answer1, $answer2);
 
+$weightCorn = ($countCorn * $answer3) / 1000000;
 
+$table = '';
+$width  = $answer1;
+$height = $answer2;
+for ($i = 1; $i <= $width ; $i++) { 
+    $table .= '<tr>';
+    for ($j = 1; $j <= $height ; $j++) { 
+        $color = ($i + $j) % 2 ? 'white' : 'black';
+        $table .= "<td bgcolor='{$color}' width='20' height='20'></td>"; 
+    }
+    $table .= '</tr>';
+}
+echo $table;
 
 ?>
+</table>
 
-<div>
-
-<h1>Задача о зёрнах на шахматной доске:</h1>
-<h3>Размер Вашей доски:
-    <?php echo "$answer1 х $answer2";
-?>
-</h3>
-<h3>Количество Ваших зёрен:   шт.</h3>
-<h3>Вес Ваших зёрен:  т.</h3>
-<h2>Созданная Вами доска:</h2>
+<h3>Размер Вашей доски: <?php echo "$answer1 х $answer2";?></h3>
+<h3>Количество Ваших зёрен: <?php echo $countCorn;?>шт.</h3>
+<h3>Вес Ваших зёрен: <?php echo $weightCorn;?>т.</h3><br>
 
 <footer>
     &copy; Zhizhnevskiy Yuriy 2021
 </footer>
-</div>
 
 </body>
 </html>
