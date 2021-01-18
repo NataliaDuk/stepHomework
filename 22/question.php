c9f1684161d1bb3571bbb4001645ab9dd3a7f521
zhizhnevskiy <zhizhnevskiy@yandex.ru>
2021-01-18 11:47:15 +0300

    doing

--- 22/question.php
+++ 22/question.php
@@ -1,33 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Developer</title>
     <style>
-        body{
-           text-align: center;
+        body {
+            text-align: center;
         }
     </style>
 </head>
+
 <body>
+
+    <?php
+
+    $data = file("file.txt");
+
+    $answer = $_POST["answer"];
+
+    $data[$answer] = ($data[$answer] + 1) . "\n";
+
+    $saveData = $data[0] . $data[1] . $data[2] . $data[3];
+
+    $fp = fopen("file.txt", "w+");
+    fwrite($fp, $saveData);
+    echo max($data[0], $data[1], $data[2], $data[3]);
+
+    fclose($fp);
+    ?>
+
     <h1>Voting results</h1>
+    <img src="PHP.jpg">
+    <?php echo  $data[0] ?>
+    <img src="JAVA.jpg">
+    <?php echo  $data[1] ?>
+    <br>
+    <img src="C++.jpg">
+    <?php echo  $data[2] ?>
+    <img src="PYTHON.jpg">
+    <?php echo  $data[3] ?>
+    <br>
+    <?php
 
-<?php
-
-$data = file("file.txt");
-
-$answer = $_POST["answer"];
-
-$data[$answer] = ($data[$answer] + 1)."\n";
-print_r($data);
-
-$saveData = $data[0] . $data[1] . $data[2] . $data[3];
-
-$fp = fopen("file.txt", "w+");
-fwrite($fp, $saveData);
-fclose($fp);
-?>
-
+    echo max($data[0], $data[1], $data[2], $data[3]);
+    print_r($data);
+    ?>
 </body>
+
 </html>
