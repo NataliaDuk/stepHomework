b114e594a1fb32ac914194e2da8e3933d01c41be
Zhiznevskiy_Yura <zhizhnevskiy@yandex.ru>
2021-01-30 14:57:12 +0300

    doing №26

---
+++ 26/config.php
@@ -0,0 +1,64 @@
+<?php
+$nameErr = $emailErr = $genderErr = $commentErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+    } else {
+        $name = test_input($_POST["name"]);
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+            $nameErr = "Only letters and white space allowed";
+            unset($name);
+        }  
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
+            unset($email);
+        }
+    }
+
+    if (empty($_POST["website"])) {
+        $websiteErr = "Website is required";
+    } else {
+        $website = test_input($_POST["website"]);
+        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) {
+            $websiteErr = "Invalid URL";
+            unset($website);
+        }
+    }
+
+    if (empty($_POST["comment"])) {
+        $commentErr = "Comment is required";
+    } else {
+        $comment = test_input($_POST["comment"]);
+    }
+
+    if (empty($_POST["gender"])) {
+        $genderErr = "Gender is required";
+    } else {
+        $gender = test_input($_POST["gender"]);
+    }
+}
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+if (!empty($name) & !empty($email) & !empty($website) & !empty($comment) & !empty($gender)){
+$row = "\n<----->\n" . $name . "\n" . $email .
+    "\n" . $website . "\n" . $comment . "\n" . $gender .
+     "\n" . date("d.m.Y") . "\n" . date("H:i");
+file_put_contents("quest.txt", $row, FILE_APPEND);
+}
+// header("Location: ?");
+// die();
---
+++ 26/guestbook.php
@@ -0,0 +1,64 @@
+<?php
+require_once "config.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Questbook</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="box">
+    <h2>My guestbook</h2>
+    <form method="POST" action="?">
+        Name: <input type="text" name="name" value="<?php echo $name; ?>">
+        <span class="error">* <?php echo $nameErr; ?></span>
+        <br><br>
+        E-mail: <input type="text" name="email" value="<?php echo $email; ?>">
+        <span class="error">* <?php echo $emailErr; ?></span>
+        <br><br>
+        Website: <input type="text" name="website" value="<?php echo $website; ?>">
+        <span class="error">* <?php echo $websiteErr; ?></span>
+        <br><br>
+        Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment; ?></textarea>
+        <span class="error">* <?php echo $commentErr; ?></span>
+        <br><br>
+        Gender:
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "Man") echo "checked"; ?> value="Man">Man
+        <input type="radio" name="gender" <?php if (isset($gender) && $gender == "Woman") echo "checked"; ?> value="Woman">Woman
+        <span class="error">* <?php echo $genderErr; ?></span>
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+        <br><br>
+    </form>
+    <table>
+        <caption>
+            <h2>My guest<h2>
+        </caption>
+        <tr>
+            <th>Name</th>
+            <th>E-mail</th>
+            <th>Website</th>
+            <th>Comment</th>
+            <th>Gender</th>
+            <th>Date</th>
+            <th>Time</th>
+        </tr>
+        <?php
+        $myQuest = file_get_contents("quest.txt");
+        $records = explode("<----->", $myQuest);
+        foreach ($records as $rec) {
+            $row = explode("\n", trim($rec));
+            echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td>
+            <td>$row[3]</td><td>$row[4]</td><td>$row[5]</td><td>$row[6]</td></tr>";
+        }
+        ?>
+    </table>
+    </div>
+</body>
+
+</html>
